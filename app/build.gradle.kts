12de2fe1247b0af8b4ed349004e775d04f55b5cd
Ola-Mik <aleksandra.mikol99@gmail.com>
2024-06-06 19:28:30 +0200

    Notifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.annotation)
     implementation(libs.activity)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.messaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
